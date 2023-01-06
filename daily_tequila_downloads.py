56b0a895856e78280c8a8d05559c8691e31e19e7
Bartłomiej Rudecki <ejbtrd@gmail.com>
2023-01-06 21:44:20 +0100

    Convert `deviceresponse.status_code` to string

--- daily_tequila_downloads.py
+++ daily_tequila_downloads.py
@@ -55,7 +55,7 @@ async def main():
             deviceresponse = requests.get(url)
 
             if deviceresponse.status_code != 200:
-                print("Failed to get data for " + device + ": " + deviceresponse.status_code)
+                print("Failed to get data for " + device + ": " + str(deviceresponse.status_code))
                 skippeddevices.append(device)
                 continue
 
